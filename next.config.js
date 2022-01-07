d9d8a30e9dbc849564addb1019de48195ce8dce1
csmurillo <csmurillo00@gmail.com>
2022-01-06 21:30:55 -0800

    githubpages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  env: {
+    PUBLIC_URL: "https://csmurillo.github.io/frontend-mentor-base-apparel",
+    assetPrefix: './'
+  },
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
 }
