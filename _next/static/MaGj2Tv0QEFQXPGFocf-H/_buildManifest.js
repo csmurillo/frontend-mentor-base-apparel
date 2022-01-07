a555c526bfd819b022c154517e7168fbe2314ed4
csmurillo <csmurillo00@gmail.com>
2022-01-06 22:00:51 -0800

    Updates

--- _next/static/MaGj2Tv0QEFQXPGFocf-H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-831f9506594e6f46.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
